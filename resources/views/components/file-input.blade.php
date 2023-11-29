af754a2d7623671152928cc0baf1f25e4888550a
munkhorgil <munkhorgil@gerege.mn>
2023-11-29 16:11:42 +0800

    file fix

--- resources/views/components/file-input.blade.php
+++ resources/views/components/file-input.blade.php
@@ -1,3 +1,5 @@
 @props(['disabled' => false])
 
-<input type="file" name="picture" $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'p-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input type="file" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' => 'p-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm',
+]) !!}>
