226d909efc22f58b227c56019b73240d2bf977f3
anted <antedace50@gmail.com>
2023-11-30 19:26:25 +0800

    email verification

---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,49 @@
+<x-mail::message>
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# @lang('Whoops!')
+@else
+# @lang('Сайн байна уу!')
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    $color = match ($level) {
+        'success', 'error' => $level,
+        default => 'primary',
+    };
+?>
+<x-mail::button :url="$actionUrl" :color="$color">
+{{ $actionText }}
+</x-mail::button>
+@endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+@lang('Номын сан'),<br>
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+
+@endisset
+</x-mail::message>
